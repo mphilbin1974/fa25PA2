4050d60cf8ded1184d923888ff120242c26765b6
Matthew Philbin <mphilbin1974@sdsu.edu>
2025-10-25 16:42:49 -0700

    heap.h: - implemented upheap()

--- heap.h
+++ heap.h
@@ -31,8 +31,21 @@ struct MinHeap {
         return root; // placeholder
     }
 
+    // Parent: (i - 1) / 2
+    // Children: 2*i + 1 & 2*i + 2.
+
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        // Completed TO DO: swap child upward while smaller than parent
+        while (pos > 0) { // each successive position is the parent of the last
+            int idx = data[pos];
+            int parentPos = (pos - 1) / 2;
+            int parentIdx = data[parentPos];
+
+            if (weightArr[idx] >= weightArr[parentIdx]) break;
+            data[pos] = parentIdx;
+            data[parentPos] = idx;
+            pos = parentPos;
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
